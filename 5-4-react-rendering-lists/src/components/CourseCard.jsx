dde93fda5744861d0f0dce22dcc6569925649cb0
LenaAshqar <s202282660@kfupm.edu.sa>
2025-10-08 11:35:06 +0300

    Task 3 Part A-2

--- 5-4-react-rendering-lists/src/components/CourseCard.jsx
+++ 5-4-react-rendering-lists/src/components/CourseCard.jsx
@@ -32,17 +32,23 @@ export default function CourseCard({ course, index, onMutateCourse }) {
       <header className="cardHeader">
         <h2>{course.title}</h2>
         {/* 🟩 PART A (Anchor): Show "All caught up" badge when ALL tasks are done (logical &&) */}
+          {course.tasks.length !== 0 && <p>All caught up</p>}
       </header>
 
 
       {/* 🟩 PART A (Anchor): If NO tasks → show message; ELSE → render the list (ternary ?: ) */}
-      <section className="tasksSection">
-        {/* 📘 TASK 2 — Render Tasks for Each Course */}
-        {/* 🔎 Anchor: You’ll write your code right inside this list. */}
-        <ul className="tasks">
-            {course.tasks.map(task => <TaskItem key={task.id} task={task} onToggle={toggleTask} onDelete={deleteTask} />)}
-        </ul>
-      </section>
+      {course.tasks.length === 0 ?
+          (<p>No tasks yet. Add your first one below</p>)
+          :
+          (<section className="tasksSection">
+              {/* 📘 TASK 2 — Render Tasks for Each Course */}
+              {/* 🔎 Anchor: You’ll write your code right inside this list. */}
+              <ul className="tasks">
+                  {course.tasks.map(task => <TaskItem key={task.id} task={task} onToggle={toggleTask} onDelete={deleteTask} />)}
+              </ul>
+          </section>)
+      }
+
 
 
       {/* Add Form (provided) */}
