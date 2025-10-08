4b7806eef5a810e7a51027b6c402f62bf062060f
LenaAshqar <s202282660@kfupm.edu.sa>
2025-10-08 11:41:07 +0300

    Task 3 Part A-1

--- 5-4-react-rendering-lists/src/components/CourseCard.jsx
+++ 5-4-react-rendering-lists/src/components/CourseCard.jsx
@@ -32,7 +32,10 @@ export default function CourseCard({ course, index, onMutateCourse }) {
       <header className="cardHeader">
         <h2>{course.title}</h2>
         {/* 🟩 PART A (Anchor): Show "All caught up" badge when ALL tasks are done (logical &&) */}
-          {course.tasks.every(task => task.isDone) && <span className="badge">All caught up!</span>}
+          {course.tasks.length !== 0 && (
+              course.tasks.every(task => task.isDone) && <span className="badge">All caught up!</span>
+          )}
+
       </header>
 
 
